31e4b685cb441e7581552dd88a76d565d78e80da
ETIFHL <ashetifhl@gmail.com>
2021-07-14 17:58:29 +0800

    docs(myPromise.js): add comment。

--- myPromise.js
+++ myPromise.js
@@ -11,23 +11,25 @@ class MyPromise {
   REJECTED_CALLBACK_LIST = []
 
   constructor(fn) {
-    try{
+    try {
+      // this的指向为当前被实例化的对象，可以拿到 status，value和reason。
       fn(this.resolve.bind(this), this.reject.bind(this))
-    }catch (e) {
+    } catch (e) {
       this.reject(e)
     }
   }
 
   isFunction(v) {
-    return typeof v === 'function';
+    return typeof v === 'function'
   }
 
   catch(onRejected) {
     return this.then(null, onRejected)
   }
 
+  // Promise.resolve 返回的是一个Promise
   static resolve(value) {
-    if(value instanceof MyPromise) {
+    if (value instanceof MyPromise) {
       return value
     }
     return new MyPromise((resolve) => {
@@ -35,12 +37,14 @@ class MyPromise {
     })
   }
 
+  // Promise.reject 也是
   static reject(reason) {
     return new MyPromise((resolve, reject) => {
       reject(reason)
     })
   }
 
+  // 执行后回调resolve
   resolve(value) {
     if (this.status === PENDING) {
       this.value = value
@@ -63,23 +67,37 @@ class MyPromise {
     }
   }
 
+  /**
+   * then(data => {}, e => {}) 一般是这样调用
+   * @param onFulfilled
+   * @param onRejected
+   * @returns {MyPromise}
+   */
   then(onFulfilled, onRejected) {
+    // 参数如果传递的不是函数，就直接搞一个返回对应值的函数即可
     const currentOnFulfilled = (typeof onFulfilled === 'function') ? onFulfilled : ((value) => value)
     const currentOnRejected = (typeof onRejected === 'function') ? onRejected : (reason => { throw reason })
+
+    // then 返回的是一个新的 promise
     const promise2 = new MyPromise((resolve, reject) => {
+      // 事件机制的封装
       const fulfilledTask = () => {
         queueMicrotask(() => {
-          try{
+          try {
+            // 外部传入的函数返回值可能是乱七八糟的东西
             const x = currentOnFulfilled(this.value)
+            // 统一 resolvePromise 处理一下, 参数为 新的 promise，传入方法的返回值，promise2 的 resolve， promise2 的 reject
             this.resolvePromise(promise2, x, resolve, reject)
           } catch (e) {
+            // 执行出错了当然 reject
             reject(e)
           }
         })
       }
+      // 同上
       const rejectedTask = () => {
         queueMicrotask(() => {
-          try{
+          try {
             const x = currentOnRejected(this.reason)
             this.resolvePromise(promise2, x, resolve, reject)
           } catch (e) {
@@ -87,57 +105,85 @@ class MyPromise {
           }
         })
       }
-      switch (this.status) {
+      // 当前状态判断下，要是异步操作没搞完，就不急着执行 then 后面的方法，放入一个 callbackList 中
+      switch ( this.status ) {
         case FULFILLED:
           fulfilledTask()
-          break;
+          break
         case REJECTED:
           rejectedTask()
-          break;
+          break
         case PENDING:
           this.FULFILLED_CALLBACK_LIST.push(fulfilledTask)
           this.REJECTED_CALLBACK_LIST.push(rejectedTask)
       }
     })
+    // 构造完了返回呗，一个新的 Promise
     return promise2
   }
 
+  /**
+   * 对返回值做处理，后面的 then 和 catch 之类的 onFulfilled 的参数期望拿到一个 value 而不是 Promise
+   *        ↓ 就是它
+   * .then(data => {})
+   * 统一 resolvePromise 处理一下, 参数为 新的 promise，传入方法的返回值，promise2 的 resolve， promise2 的 reject
+   * @param promise2
+   * @param x
+   * @param resolve
+   * @param reject
+   * @returns {*}
+   */
   resolvePromise(promise2, x, resolve, reject) {
-    if(promise2 === x) {
+    /**
+     * 如果promise2 === x，会造成递归。
+     * 像这样瞎搞就会出问题
+     * .then 的 onFulfilled 返回的 Promise (这个就是x) 是 .then 生成的 Promise (这个就是Promise2，它被赋值给了aaa)
+     * var aaa = Promise.resolve(123).then(function() {
+     *   return aaa
+     * })
+     */
+    if (promise2 === x) {
       return reject(new TypeError('promise is same as return value'))
     }
-    if(x instanceof MyPromise) {
+    if (x instanceof MyPromise) {
+      // onFulfilled 又返回了一个 Promise，继续 resolve 即可, resolve 和 reject 还是 then 里的 promise2 的。
       x.then(y => {
         this.resolvePromise(promise2, y, resolve, reject)
       }, reject)
-    } else if(typeof x === 'object' || typeof x === 'function') {
-      if(x === null) {
+    } else if (typeof x === 'object' || typeof x === 'function') {
+      // typeof null === 'object' 是一个 feature！
+      if (x === null) {
         return resolve(x)
       }
       let then = null
-      try{
+      // 规范这么定的，为了兼容。(有 then 就能当 promise 了吗？)
+      try {
         then = x.then
       } catch (e) {
+        // 取个值也有能报错，可见 js 的灵活性，Object.defineProperty, proxy 之类的，还有啥奇淫巧技以后想起来再加
         return reject(e)
       }
       if (typeof then === 'function') {
+        // 整个 flag 防止重复调用
         let called = false
         try {
+          // 终于调用了，别管外部 then 方法绑啥乱七八糟的 context。
+          // 这里
           then.call(
             x,
             (y) => {
-              if(called) return
-              called = true
+              if (called) return
+              // called = true
               this.resolvePromise(promise2, y, resolve, reject)
             },
             (r) => {
-              if(called) return
-              called = true
+              if (called) return
+              // called = true
               reject(r)
             }
           )
         } catch (e) {
-          if(called) return
+          if (called) return
           reject(e)
         }
       } else {
@@ -157,5 +203,11 @@ class MyPromise {
     return result
   }
 }
+
+/**
+ * 实现搞下来，这玩意本质上是一个回调的处理方法。
+ * 非异步的话就是普通的链式调用。
+ * 异步情况就把后续的逻辑给放到一个回调数组中，等待完成后调用。
+ */
 
 module.exports = MyPromise
